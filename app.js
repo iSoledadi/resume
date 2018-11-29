89176b6f694a4600a7cd0569cfecb54daf58352d
iSoledadi <838027059@qq.com>
2018-11-30 01:35:29 +0800

    Add files via upload
    app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+/*引入路由模块*/
+
+
+var app = express();
+var server = app.listen(7777);
+//使用body-parser中间件
+app.use(bodyParser.urlencoded({extended:false}));
+//托管静态资源到public目录下
+app.use(express.static('BILLZCON3'));
