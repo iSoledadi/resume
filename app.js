bf508366c062d23ca3183a12c9522eca19ae82ed
ZhongJiaNing <838027059@qq.com>
2018-12-02 23:35:04 +0800

    blizzcon-v1.1 add blizzcon store

--- app.js
+++ app.js
@@ -8,4 +8,4 @@ var server = app.listen(7777);
 //使用body-parser中间件
 app.use(bodyParser.urlencoded({extended:false}));
 //托管静态资源到public目录下
-app.use(express.static('BILLZCON3'));
+app.use(express.static('resume'));
